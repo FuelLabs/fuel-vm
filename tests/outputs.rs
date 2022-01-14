adb520f1f435d34c1801ca93ba0ed53d6021dddb
Brandon Kite <brandonkite92@gmail.com>
2022-01-13 19:19:19 -0800

    add tests for transfer opcode

--- tests/outputs.rs
+++ tests/outputs.rs
@@ -1,7 +1,22 @@
-use fuel_vm::consts::REG_ONE;
+use fuel_vm::consts::{REG_ONE, REG_ZERO, VM_TX_MEMORY};
 use fuel_vm::prelude::*;
+use itertools::Itertools;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
+
+macro_rules! script_with_offset {
+    ($offset:ident, $script:expr) => {{
+        use fuel_types::bytes;
+        use fuel_vm::consts::VM_TX_MEMORY;
+        let $offset = 0;
+        let script_bytes: Vec<u8> = { $script }.into_iter().collect();
+        let data_offset = VM_TX_MEMORY + Transaction::script_offset() + bytes::padded_len(script_bytes.as_slice());
+        let $offset = data_offset;
+        {
+            $script
+        }
+    }};
+}
 
 /// Testing of post-execution output handling
 
@@ -158,22 +173,139 @@ fn change_is_not_duplicated_for_each_base_asset_change_output() {
     assert_eq!(total_change, input_amount);
 }
 
-// TODO: implement these test cases when TR opcode is supported
 #[test]
-#[ignore]
-fn base_asset_change_is_reduced_by_contract_transfer() {}
+fn change_is_reduced_by_external_transfer() {
+    let rng = &mut StdRng::seed_from_u64(2322u64);
+
+    let input_amount = 1000;
+    let transfer_amount: Word = 400;
+    let gas_price = 0;
+    let gas_limit = 1_000_000;
+    let byte_price = 0;
+    let asset_id = Color::default();
+
+    // setup state for test
+    let mut storage = MemoryStorage::default();
+    // simple dummy contract for transferring value to
+    let contract_code = vec![Opcode::RET(REG_ONE)].into_iter().collect::<Vec<u8>>();
+    let contract = Contract::from(contract_code.as_ref());
+    let salt: Salt = rng.gen();
+    let contract_root = contract.root();
+    let contract_id = contract.id(&salt, &contract_root);
+    storage.storage_contract_insert(&contract_id, &contract).unwrap();
+    storage
+        .storage_contract_root_insert(&contract_id, &salt, &contract_root)
+        .unwrap();
+
+    // setup script for transfer
+    let script = script_with_offset!(
+        data_offset,
+        vec![
+            // set reg 0x10 to contract id
+            Opcode::ADDI(0x10, REG_ZERO, data_offset as Immediate12),
+            // set reg 0x11 to transfer amount
+            Opcode::ADDI(0x11, REG_ZERO, transfer_amount as Immediate12),
+            // set reg 0x12 to color
+            Opcode::ADDI(0x12, REG_ZERO, (data_offset + 32) as Immediate12),
+            // transfer to contract ID at 0x10, the amount of coins at 0x11, of the color at 0x12
+            Opcode::TR(0x10, 0x11, 0x12),
+            Opcode::RET(REG_ONE),
+        ]
+    );
+
+    let script_data = [contract_id.as_ref(), asset_id.as_ref()]
+        .into_iter()
+        .flatten()
+        .copied()
+        .collect();
+
+    // execute and get change
+    let change = TestBuilder::new(2322u64)
+        .gas_price(gas_price)
+        .gas_limit(gas_limit)
+        .byte_price(byte_price)
+        .coin_input(asset_id, input_amount)
+        .contract_input(contract_id)
+        .change_output(asset_id)
+        .contract_output(&contract_id)
+        .script(script)
+        .script_data(script_data)
+        .execute_get_change(asset_id);
+
+    assert_eq!(change, input_amount - transfer_amount);
+}
+
+#[test]
+fn change_is_not_reduced_by_external_transfer_on_revert() {
+    let rng = &mut StdRng::seed_from_u64(2322u64);
+
+    let input_amount = 1000;
+    // attempt overspend to cause a revert
+    let transfer_amount: Word = input_amount + 100;
+    let gas_price = 0;
+    let gas_limit = 1_000_000;
+    let byte_price = 0;
+    let maturity = 0;
+    let asset_id = Color::default();
+
+    // setup state for test
+    let mut storage = MemoryStorage::default();
+    // simple dummy contract for transferring value to
+    let contract_code = vec![Opcode::RET(REG_ONE)].into_iter().collect::<Vec<u8>>();
+    let contract = Contract::from(contract_code.as_ref());
+    let salt: Salt = rng.gen();
+    let contract_root = contract.root();
+    let contract_id = contract.id(&salt, &contract_root);
+    storage.storage_contract_insert(&contract_id, &contract).unwrap();
+    storage
+        .storage_contract_root_insert(&contract_id, &salt, &contract_root)
+        .unwrap();
+
+    // setup script for transfer
+    let script = script_with_offset!(
+        data_offset,
+        vec![
+            // set reg 0x10 to contract id
+            Opcode::ADDI(0x10, REG_ZERO, data_offset as Immediate12),
+            // set reg 0x11 to transfer amount
+            Opcode::ADDI(0x11, REG_ZERO, transfer_amount as Immediate12),
+            // set reg 0x12 to color
+            Opcode::ADDI(0x12, REG_ZERO, (data_offset + 32) as Immediate12),
+            // transfer to contract ID at 0x10, the amount of coins at 0x11, of the color at 0x12
+            Opcode::TR(0x10, 0x11, 0x12),
+            Opcode::RET(REG_ONE),
+        ]
+    );
+
+    let script_data = [contract_id.as_ref(), asset_id.as_ref()]
+        .into_iter()
+        .flatten()
+        .copied()
+        .collect();
+
+    // execute and get change
+    let change = TestBuilder::new(2322u64)
+        .gas_price(gas_price)
+        .gas_limit(gas_limit)
+        .byte_price(byte_price)
+        .coin_input(asset_id, input_amount)
+        .contract_input(contract_id)
+        .change_output(asset_id)
+        .contract_output(&contract_id)
+        .script(script)
+        .script_data(script_data)
+        .execute_get_change(asset_id);
+
+    assert_eq!(change, input_amount);
+}
 
 #[test]
 #[ignore]
-fn base_asset_change_is_not_reduced_by_contract_transfer_on_revert() {}
+fn variable_output_increased_by_contract_transfer_out() {}
 
 #[test]
 #[ignore]
-fn asset_change_reduced_by_contract_transfer() {}
-
-#[test]
-#[ignore]
-fn asset_change_not_reduced_by_contract_transfer_on_revert() {}
+fn variable_output_not_increased_by_contract_transfer_out_on_revert() {}
 
 pub struct TestBuilder {
     rng: StdRng,
@@ -185,6 +317,7 @@ pub struct TestBuilder {
     script: Vec<Opcode>,
     script_data: Vec<u8>,
     witness: Vec<Witness>,
+    storage: MemoryStorage,
 }
 
 impl TestBuilder {
@@ -199,11 +332,17 @@ impl TestBuilder {
             script: vec![Opcode::RET(REG_ONE)],
             script_data: vec![],
             witness: vec![Witness::default()],
+            storage: MemoryStorage::default(),
         }
     }
 
     pub fn gas_price(&mut self, price: Word) -> &mut TestBuilder {
         self.gas_price = price;
+        self
+    }
+
+    pub fn gas_limit(&mut self, limit: Word) -> &mut TestBuilder {
+        self.gas_limit = limit;
         self
     }
 
@@ -232,6 +371,17 @@ impl TestBuilder {
         self
     }
 
+    pub fn contract_output(&mut self, id: &ContractId) -> &mut TestBuilder {
+        let input_idx = self
+            .inputs
+            .iter()
+            .find_position(|input| matches!(input, Input::Contract {contract_id, ..} if contract_id == id))
+            .expect("expected contract input with matching contract id");
+        self.outputs
+            .push(Output::contract(input_idx.0 as u8, self.rng.gen(), self.rng.gen()));
+        self
+    }
+
     pub fn coin_input(&mut self, color: Color, amount: Word) -> &mut TestBuilder {
         self.inputs.push(Input::coin(
             self.rng.gen(),
@@ -242,6 +392,16 @@ impl TestBuilder {
             0,
             vec![],
             vec![],
+        ));
+        self
+    }
+
+    pub fn contract_input(&mut self, contract_id: ContractId) -> &mut TestBuilder {
+        self.inputs.push(Input::contract(
+            self.rng.gen(),
+            self.rng.gen(),
+            self.rng.gen(),
+            contract_id,
         ));
         self
     }
@@ -261,6 +421,11 @@ impl TestBuilder {
         self
     }
 
+    pub fn storage(&mut self, storage: MemoryStorage) -> &mut TestBuilder {
+        self.storage = storage;
+        self
+    }
+
     pub fn build(&mut self) -> Transaction {
         Transaction::script(
             self.gas_price,
@@ -277,10 +442,26 @@ impl TestBuilder {
 
     pub fn execute(&mut self) -> Vec<Output> {
         let tx = self.build();
-        let mut client = MemoryClient::default();
+        let mut client = MemoryClient::new(self.storage.clone());
         client.transact(tx);
         let txtor: Transactor<_> = client.into();
         let outputs = txtor.state_transition().unwrap().tx().outputs();
         outputs.to_vec()
+    }
+
+    pub fn execute_get_change(&mut self, find_color: Color) -> Word {
+        let outputs = self.execute();
+        let change = outputs.into_iter().find_map(|output| {
+            if let Output::Change { amount, color, .. } = output {
+                if &color == &find_color {
+                    Some(amount)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        });
+        change.expect(format!("no change matching color {:x} was found", &find_color).as_str())
     }
 }

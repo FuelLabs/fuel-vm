0078fd27e04fcb1899f2e9140fb09e1861a2d712
Brandon Kite <brandonkite92@gmail.com>
2022-04-01 09:11:30 -0700

    make docs feature aware on docs.rs (#21)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! Fuel cryptographic primitives.
 
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![warn(missing_docs)]
 // Wrong clippy convention; check

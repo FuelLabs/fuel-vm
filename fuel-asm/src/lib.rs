84d787c947756bdcd2b897d6c6d4936a73b7305f
Brandon Kite <brandonkite92@gmail.com>
2022-05-02 10:30:52 -0700

    Re-export deps used in public interfaces + docs improvements (#59)
    * fully re-export crates used in public interfaces and improve doc rendering

--- fuel-asm/src/lib.rs
+++ fuel-asm/src/lib.rs
@@ -1,5 +1,6 @@
 //! FuelVM opcodes representation
 
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "std", doc = include_str!("../README.md"))]
 #![warn(missing_docs)]
@@ -10,7 +11,11 @@ mod panic_reason;
 
 mod macros;
 
-pub use fuel_types::{Immediate06, Immediate12, Immediate18, Immediate24, RegisterId, Word};
+#[doc(no_inline)]
+pub use fuel_types::{self, Immediate06, Immediate12, Immediate18, Immediate24, RegisterId, Word};
 pub use instruction::Instruction;
 pub use opcode::{Opcode, OpcodeRepr};
 pub use panic_reason::{InstructionResult, PanicReason};
+#[cfg(feature = "serde-types-minimal")]
+#[doc(no_inline)]
+pub use serde;

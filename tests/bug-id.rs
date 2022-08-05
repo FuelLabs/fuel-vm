7b06e363eedcd29427958738b153d6471ba36257
Victor Lopez <vhrlopes@gmail.com>
2022-08-05 18:48:56 +0200

    Add format check

--- tests/bug-id.rs
+++ tests/bug-id.rs
@@ -11,10 +11,15 @@ use fuel_vm::prelude::*;
 fn check_bug_id_unique() {
     let mut matches = HashSet::new();
     let re = Regex::new(r"BugId::ID\d{3}").expect("failed to create regex");
+    let re_use = Regex::new(r"BugId::\*").expect("failed to create regex");
 
     for source in glob("src/**/*.rs").expect("Failed to read glob pattern") {
         let source = source.expect("failed to fetch source from glob");
         let source = fs::read_to_string(source).expect("failed to read source");
+
+        if re_use.is_match(&source) {
+            panic!("BugId isn't supposed to be required as '*'")
+        }
 
         re.find_iter(&source).map(|m| m.as_str().to_string()).for_each(|s| {
             if !matches.insert(s.clone()) {
@@ -24,7 +29,13 @@ fn check_bug_id_unique() {
     }
 
     BugId::VARIANTS.iter().for_each(|v| {
-        if !matches.contains(&format!("BugId::{}", v)) {
+        let v = format!("BugId::{}", v);
+
+        if !re.is_match(&v) {
+            panic!("the bug id format is inconsistent: {}", v);
+        }
+
+        if !matches.contains(&v) {
             panic!("the bug id variant is never constructed: {}", v);
         }
     });

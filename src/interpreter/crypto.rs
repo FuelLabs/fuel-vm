dc42a78615867801a77f1553cd2b458ae2c3e9d1
Mitch Martin <mitch.martin@fuel.sh>
2022-09-16 08:50:12 -0500

    use new arith module for crypto.rs
    Signed-off-by: Mitch Martin <mitch.martin@fuel.sh>

--- src/interpreter/crypto.rs
+++ src/interpreter/crypto.rs
@@ -1,7 +1,8 @@
 use super::Interpreter;
-use crate::consts::{MEM_MAX_ACCESS_SIZE, VM_MAX_RAM};
+use crate::consts::{MEM_MAX_ACCESS_SIZE, MIN_VM_MAX_RAM_USIZE_MAX, VM_MAX_RAM};
 use crate::error::RuntimeError;
 
+use crate::arith::checked_add_word;
 use fuel_asm::PanicReason;
 use fuel_asm::PanicReason::ArithmeticOverflow;
 use fuel_crypto::{Hasher, Message, PublicKey, Signature};
@@ -9,15 +10,10 @@ use fuel_types::{Bytes32, Bytes64, Word};
 
 impl<S> Interpreter<S> {
     pub(crate) fn ecrecover(&mut self, a: Word, b: Word, c: Word) -> Result<(), RuntimeError> {
-        let bx = b.checked_add(Bytes64::LEN as Word).ok_or_else(|| ArithmeticOverflow)?;
-        let cx = c.checked_add(Bytes32::LEN as Word).ok_or_else(|| ArithmeticOverflow)?;
+        let bx = checked_add_word(b, Bytes64::LEN as Word)?;
+        let cx = checked_add_word(c, Bytes32::LEN as Word)?;
 
-        if a > VM_MAX_RAM - Bytes64::LEN as Word
-            || bx > usize::MAX as Word
-            || bx > VM_MAX_RAM
-            || cx > usize::MAX as Word
-            || cx > VM_MAX_RAM
-        {
+        if a > VM_MAX_RAM - Bytes64::LEN as Word || bx > MIN_VM_MAX_RAM_USIZE_MAX || cx > MIN_VM_MAX_RAM_USIZE_MAX {
             return Err(PanicReason::MemoryOverflow.into());
         }
 
@@ -44,13 +40,9 @@ impl<S> Interpreter<S> {
     pub(crate) fn keccak256(&mut self, a: Word, b: Word, c: Word) -> Result<(), RuntimeError> {
         use sha3::{Digest, Keccak256};
 
-        let bc = b.checked_add(c).ok_or_else(|| ArithmeticOverflow)?;
+        let bc = checked_add_word(b, c)?;
 
-        if a > VM_MAX_RAM - Bytes32::LEN as Word
-            || c > MEM_MAX_ACCESS_SIZE
-            || bc > usize::MAX as Word
-            || bc > VM_MAX_RAM
-        {
+        if a > VM_MAX_RAM - Bytes32::LEN as Word || c > MEM_MAX_ACCESS_SIZE || bc > MIN_VM_MAX_RAM_USIZE_MAX {
             return Err(PanicReason::MemoryOverflow.into());
         }
 
@@ -66,13 +58,9 @@ impl<S> Interpreter<S> {
     }
 
     pub(crate) fn sha256(&mut self, a: Word, b: Word, c: Word) -> Result<(), RuntimeError> {
-        let bc = b.checked_add(c).ok_or_else(|| ArithmeticOverflow)?;
+        let bc = checked_add_word(b, c)?;
 
-        if a > VM_MAX_RAM - Bytes32::LEN as Word
-            || c > MEM_MAX_ACCESS_SIZE
-            || bc > usize::MAX as Word
-            || bc > VM_MAX_RAM
-        {
+        if a > VM_MAX_RAM - Bytes32::LEN as Word || c > MEM_MAX_ACCESS_SIZE || bc > MIN_VM_MAX_RAM_USIZE_MAX {
             return Err(PanicReason::MemoryOverflow.into());
         }
 

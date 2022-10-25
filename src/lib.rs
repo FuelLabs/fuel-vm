6b6134d1be83b4331e718e09685ddd1436fbfd4e
Brandon Kite <brandonkite92@gmail.com>
2022-10-25 13:44:54 -0700

    Re-export mnemonic deps (#33)
    * rexport mnemonic deps used for keygen
    
    * rexport mnemonic deps
    
    * alphabetize exports

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,14 @@
 /// Required export to implement [`Keystore`].
 #[doc(no_inline)]
 pub use borrown;
+/// Required export for using mnemonic keygen on [`SecretKey::new_from_mnemonic`]
+#[cfg(feature = "std")]
+#[doc(no_inline)]
+pub use coins_bip32;
+/// Required export for using mnemonic keygen on [`SecretKey::new_from_mnemonic`]
+#[cfg(feature = "std")]
+#[doc(no_inline)]
+pub use coins_bip39;
 /// Required export to use various public interfaces in this crate
 #[doc(no_inline)]
 pub use fuel_types;

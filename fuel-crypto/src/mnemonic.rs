321a674aa208a60eeb770636b6a9a7df1f28e13f
Tom <tomrgowan@gmail.com>
2023-01-04 15:39:57 +1100

    Fix features (#296)
    fix compile errors for default crate features and add checks to ci

--- fuel-crypto/src/mnemonic.rs
+++ fuel-crypto/src/mnemonic.rs
@@ -1,7 +1,7 @@
 /// FuelMnemonic is a simple mnemonic phrase generator.
 pub struct FuelMnemonic;
 
-#[cfg(feature = "std")]
+#[cfg(all(feature = "std", feature = "random"))]
 mod use_std {
     use super::FuelMnemonic;
     use crate::Error;
@@ -9,13 +9,11 @@ mod use_std {
 
     pub type W = English;
 
-    #[cfg(feature = "random")]
     use rand::Rng;
 
     impl FuelMnemonic {
         /// Generates a random mnemonic phrase given a random number generator and
         /// the number of words to generate, `count`.
-        #[cfg(feature = "random")]
         pub fn generate_mnemonic_phrase<R: Rng>(rng: &mut R, count: usize) -> Result<String, Error> {
             Ok(Mnemonic::<W>::new_with_count(rng, count)?.to_phrase()?)
         }

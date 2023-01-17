2b8281a9f4b27f1aff3ea3b1feba87ebdb4ea4fa
Hannes Karppila <hannes.karppila@gmail.com>
2023-01-17 22:23:46 +0200

    Fix extra import after PR merge

--- fuel-vm/tests/predicate.rs
+++ fuel-vm/tests/predicate.rs
@@ -242,8 +242,6 @@ fn gas_used_by_predicates_is_deducted_from_script_gas() {
 
 #[test]
 fn gas_used_by_predicates_causes_out_of_gas() {
-    use fuel_vm::predicate::CheckedTxExt;
-
     let rng = &mut StdRng::seed_from_u64(2322u64);
 
     let gas_price = 1_000;

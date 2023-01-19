427de9b30a16c13e853ea114250664075fb74938
Hannes Karppila <hannes.karppila@gmail.com>
2023-01-19 22:47:23 +0200

    Make Finalizable trait, a generic way to call TransactionBuilder::finalize

--- fuel-vm/src/checked_transaction/builder.rs
+++ fuel-vm/src/checked_transaction/builder.rs
@@ -16,42 +16,20 @@ where
     fn finalize_checked_basic(&mut self, height: Word, params: &ConsensusParameters) -> Checked<Tx>;
 }
 
-impl TransactionBuilderExt<Create> for TransactionBuilder<Create> {
-    fn finalize_checked(&mut self, height: Word, params: &ConsensusParameters, gas_costs: GasCosts) -> Checked<Create> {
+impl<Tx: IntoChecked + ExecutableTransaction + fuel_tx::field::GasLimit> TransactionBuilderExt<Tx>
+    for TransactionBuilder<Tx>
+where
+    Checked<Tx>: Clone,
+    TransactionBuilder<Tx>: Finalizable<Tx>,
+    <Tx as IntoChecked>::Metadata: crate::interpreter::CheckedMetadata,
+{
+    fn finalize_checked(&mut self, height: Word, params: &ConsensusParameters, gas_costs: GasCosts) -> Checked<Tx> {
         self.finalize()
             .into_checked(height, params, gas_costs)
             .expect("failed to check tx")
     }
 
-    fn finalize_checked_basic(&mut self, height: Word, params: &ConsensusParameters) -> Checked<Create> {
-        self.finalize()
-            .into_checked_basic(height, params)
-            .expect("failed to check tx")
-    }
-}
-
-impl TransactionBuilderExt<Mint> for TransactionBuilder<Mint> {
-    fn finalize_checked(&mut self, height: Word, params: &ConsensusParameters, gas_costs: GasCosts) -> Checked<Mint> {
-        self.finalize()
-            .into_checked(height, params, gas_costs)
-            .expect("failed to check tx")
-    }
-
-    fn finalize_checked_basic(&mut self, height: Word, params: &ConsensusParameters) -> Checked<Mint> {
-        self.finalize()
-            .into_checked_basic(height, params)
-            .expect("failed to check tx")
-    }
-}
-
-impl TransactionBuilderExt<Script> for TransactionBuilder<Script> {
-    fn finalize_checked(&mut self, height: Word, params: &ConsensusParameters, gas_costs: GasCosts) -> Checked<Script> {
-        self.finalize()
-            .into_checked(height, params, gas_costs)
-            .expect("failed to check tx")
-    }
-
-    fn finalize_checked_basic(&mut self, height: Word, params: &ConsensusParameters) -> Checked<Script> {
+    fn finalize_checked_basic(&mut self, height: Word, params: &ConsensusParameters) -> Checked<Tx> {
         self.finalize()
             .into_checked_basic(height, params)
             .expect("failed to check tx")

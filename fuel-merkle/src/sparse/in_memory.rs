2301c1adb1d2490da0e72bca1ed0c6c3289cccd0
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-02-14 13:49:18 -0500

    Fix merge

--- fuel-merkle/src/sparse/in_memory.rs
+++ fuel-merkle/src/sparse/in_memory.rs
@@ -13,11 +13,11 @@ impl Mappable for NodesTable {
     type Key = Self::OwnedKey;
     type OwnedKey = WrappedBytes32;
     type Value = Self::OwnedValue;
-    type OwnedValue = Primitive<Self::Key>;
+    type OwnedValue = Primitive;
 }
 
 type Storage = StorageMap<NodesTable>;
-type SparseMerkleTree = sparse::MerkleTree<NodesTable, Storage, WrappedBytes32>;
+type SparseMerkleTree = sparse::MerkleTree<NodesTable, Storage>;
 
 pub struct MerkleTree {
     tree: SparseMerkleTree,

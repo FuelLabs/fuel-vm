048c93237a701aa850eaafdce633d36b92981f2e
Hannes Karppila <hannes.karppila@gmail.com>
2023-02-23 17:13:07 +0200

    WIP

--- fuel-vm/tests/alu.rs
+++ fuel-vm/tests/alu.rs
@@ -2,17 +2,9 @@ use fuel_asm::RegId;
 use fuel_asm::{op, Instruction};
 use fuel_vm::prelude::*;
 
-/// Set a register `r` to a Word-sized number value using left-shifts
-fn set_full_word(r: RegisterId, v: Word) -> Vec<Instruction> {
-    let r = u8::try_from(r).unwrap();
-    let mut ops = vec![op::movi(r, 0)];
-    for byte in v.to_be_bytes() {
-        ops.push(op::ori(r, r, byte as Immediate12));
-        ops.push(op::slli(r, r, 8));
-    }
-    ops.pop().unwrap(); // Remove last shift
-    ops
-}
+mod test_helpers;
+
+use test_helpers::set_full_word;
 
 fn alu(registers_init: &[(RegisterId, Word)], ins: Instruction, reg: RegisterId, expected: Word) {
     let storage = MemoryStorage::default();

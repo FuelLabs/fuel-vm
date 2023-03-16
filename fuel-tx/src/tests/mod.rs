f0d41c9e696836780f99674bcd88724c0006b455
green <xgreenx9999@gmail.com>
2023-03-16 22:35:20 +0000

    Fix compilation for the `fuel-tx`

--- fuel-tx/src/tests/mod.rs
+++ fuel-tx/src/tests/mod.rs
@@ -1,5 +1,10 @@
-mod bytes;
-mod display;
 mod offset;
 mod prepared_init;
 mod valid_cases;
+
+#[cfg(feature = "serde")]
+mod bytes;
+#[cfg(feature = "serde")]
+mod display;
+#[cfg(not(feature = "serde"))]
+use bincode as _;

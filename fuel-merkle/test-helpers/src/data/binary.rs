90a9a9c9e3e897d5b335ed8cf69a3ed77fa8be4d
Green Baneling <XgreenX9999@gmail.com>
2023-03-22 09:43:00 +0000

    Make verification for binary merkle tree public (#405)
    * Make verification for binary merkle tree public
    
    * Make clippy happy
    
    * Return back `verify`
    
    * Add usage for both verifications
    
    * Fix compilation
    
    * Added `digest` t othe verificaiton

--- fuel-merkle/test-helpers/src/data/binary.rs
+++ fuel-merkle/test-helpers/src/data/binary.rs
@@ -1,10 +1,11 @@
+use fuel_merkle::binary::verify;
 use serde::{Deserialize, Serialize};
 use std::convert::TryInto;
 
 use fuel_merkle::common::Bytes32;
 
 use crate::{
-    binary::verify,
+    binary::verify as verify_from_test_helper,
     data::{EncodedValue, TestError},
 };
 
@@ -14,6 +15,7 @@ pub struct ProofTest {
     pub function_name: String,
     pub description: String,
     pub root: EncodedValue,
+    pub data: Bytes32,
     pub proof_set: Vec<EncodedValue>,
     pub proof_index: u64,
     pub num_leaves: u64,
@@ -30,7 +32,10 @@ impl ProofTest {
             .map(|v| v.into_bytes().unwrap().as_slice().try_into().unwrap())
             .collect::<Vec<Bytes32>>();
 
-        let verification = verify(&root, &proof_set, self.proof_index, self.num_leaves);
+        let verification = verify(&root, self.data, &proof_set, self.proof_index, self.num_leaves);
+        let verification_from_test_helper =
+            verify_from_test_helper(&root, &proof_set, self.proof_index, self.num_leaves);
+        assert_eq!(verification, verification_from_test_helper);
 
         if verification == self.expected_verification {
             Ok(())

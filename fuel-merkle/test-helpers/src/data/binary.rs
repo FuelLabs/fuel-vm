bcff5aa99ff578f31f451a26adf6a0ac0367ccde
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-03-22 12:33:43 -0400

    Refactor BMT proof data tests to use encoded value

--- fuel-merkle/test-helpers/src/data/binary.rs
+++ fuel-merkle/test-helpers/src/data/binary.rs
@@ -15,7 +15,7 @@ pub struct ProofTest {
     pub function_name: String,
     pub description: String,
     pub root: EncodedValue,
-    pub data: Bytes32,
+    pub data: EncodedValue,
     pub proof_set: Vec<EncodedValue>,
     pub proof_index: u64,
     pub num_leaves: u64,
@@ -31,8 +31,8 @@ impl ProofTest {
             .cloned()
             .map(|v| v.into_bytes().unwrap().as_slice().try_into().unwrap())
             .collect::<Vec<Bytes32>>();
-
-        let verification = verify(&root, self.data, &proof_set, self.proof_index, self.num_leaves);
+        let data = self.data.into_bytes()?;
+        let verification = verify(&root, data, &proof_set, self.proof_index, self.num_leaves);
         let verification_from_test_helper =
             verify_from_test_helper(&root, &proof_set, self.proof_index, self.num_leaves);
         assert_eq!(verification, verification_from_test_helper);

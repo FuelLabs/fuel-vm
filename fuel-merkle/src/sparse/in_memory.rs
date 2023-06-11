1adf5820894657754442ca0d6ae428c42cb0d071
green <xgreenx9999@gmail.com>
2023-06-11 01:20:54 +0100

    Make CI happy

--- fuel-merkle/src/sparse/in_memory.rs
+++ fuel-merkle/src/sparse/in_memory.rs
@@ -4,6 +4,7 @@ use crate::{
     storage::{Mappable, StorageInspect, StorageMutate},
 };
 use alloc::borrow::Cow;
+use alloc::vec::Vec;
 
 /// The table of the Sparse Merkle tree's nodes. [`MerkleTree`] works with it as a sparse merkle
 /// tree, where the storage key is `Bytes32` and the value is the [`Buffer`](crate::sparse::Buffer)

5d304dc8b8224c56cef830be703c4d1c395db459
anderson <anderson@arboleya.me>
2023-07-17 16:22:56 -0300

    Encapsulating commands as a function for optimal reuse

--- wasm_publish.sh
+++ wasm_publish.sh
@@ -1,36 +1,42 @@
 #!/usr/bin/env bash
 
-# cleaning up
-rm -rf .npm/fuel-asm/{src,dist}
+
+build_and_publish_wasm_pkg ()
+{
+  dash_name=$1
+  underscore_name=$(echo "${dash_name}" | sed -r 's/-/_/g')
+
+  rm -rf .npm/${dash_name}/{src,dist}
+
+  # building & optimizing wasm
+  cargo rustc -p ${dash_name} --target wasm32-unknown-unknown --features typescript --crate-type=cdylib --release
+  wasm-bindgen --target web ./target/wasm32-unknown-unknown/release/${underscore_name}.wasm --out-dir .npm/${dash_name}/src
+  wasm-opt .npm/${dash_name}/src/${underscore_name}_bg.wasm -o .npm/${dash_name}/src/${underscore_name}_bg.wasm -Oz
+
+  # creating entrypoint for loading everything
+cat > .npm/${dash_name}/src/index.js <<EOM
+  import init from './${underscore_name}.js'
+  import wasm from './${underscore_name}_bg.wasm'
+
+  init(wasm())
+
+  export * from './${underscore_name}.js'
+EOM
+
+  # commenting `new URL()` and `fetch()` calls (plays nice with other bundlers)
+  sed -i.bkp -r 's;(input = new URL.+);//\1;g' .npm/${dash_name}/src/${underscore_name}.js
+  sed -i.bkp -r 's;(input = fetch.+);//\1;g' .npm/${dash_name}/src/${underscore_name}.js
+
+  # removing backup file (after sed replacement)
+  rm .npm/${dash_name}/src/${underscore_name}.js.bkp
+
+  # building and testing npm package
+  pnpm -C .npm/${dash_name} install
+  pnpm -C .npm/${dash_name} build
+  pnpm -C .npm/${dash_name} test
+
+  # TODO: publish logic will go here...
+}
 
 
-# building & optimizing wasm
-cargo rustc -p fuel-asm --target wasm32-unknown-unknown --features typescript --crate-type=cdylib --release
-wasm-bindgen --target web ./target/wasm32-unknown-unknown/release/fuel_asm.wasm --out-dir .npm/fuel-asm/src
-wasm-opt .npm/fuel-asm/src/fuel_asm_bg.wasm -o .npm/fuel-asm/src/fuel_asm_bg.wasm -Oz
-
-
-# creating entrypoint for loading everything
-cat >.npm/fuel-asm/src/index.js <<EOL
-import init from './fuel_asm.js'
-import wasm from './fuel_asm_bg.wasm'
-
-init(wasm())
-
-export * from './fuel_asm.js'
-EOL
-
-
-# commenting `new URL()` and `fetch()` calls (plays nice with other bundlers)
-sed -i.bkp -r 's;(input = new URL.+);//\1;g' .npm/fuel-asm/src/fuel_asm.js
-sed -i.bkp -r 's;(input = fetch.+);//\1;g' .npm/fuel-asm/src/fuel_asm.js
-
-
-# removing backup file (after sed replacement)
-rm .npm/fuel-asm/src/fuel_asm.js.bkp
-
-
-# building and testing npm package
-pnpm -C .npm/fuel-asm install
-pnpm -C .npm/fuel-asm build
-pnpm -C .npm/fuel-asm test
+build_and_publish_wasm_pkg "fuel-asm"

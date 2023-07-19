ea688c90011f81678a39ffcfba735bcf0f297842
anderson <anderson@arboleya.me>
2023-07-19 10:50:25 -0300

    Adjusting templates, using `@fuels/` org prefix for NPM packages

--- .npm/.pkg-template/README.md
+++ .npm/.pkg-template/README.md
@@ -2,4 +2,5 @@
 # {{NAME_DASHED}}
 
 WASM version of `{{NAME_DASHED}}` Rust crate:
+ - https://crates.io/crates/{{NAME_DASHED}}
  - https://github.com/FuelLabs/fuel-vm/tree/master/{{NAME_DASHED}}
--- .npm/build_wasm_pkgs.sh
+++ .npm/build_wasm_pkgs.sh
@@ -11,16 +11,18 @@ write_template ()
   TEMPLATE=$3
 
   PKG_TEMPLATE_DIR=${NPM_DIR}/.pkg-template
-  PACKAGE_DIR=${NPM_DIR}/${NAME_DASHED}
+  PKG_DIR=${NPM_DIR}/${NAME_DASHED}
 
-  VERSION=$(cat ${ROOT_DIR}/Cargo.toml | sed -nr 's/^version = "([^"]+)"/\1/p')
+  PKG_NAME=$(echo "${NAME_DASHED}" | sed -r 's/fuel-//g')
+  PKG_VERSION=$(cat ${ROOT_DIR}/Cargo.toml | sed -nr 's/^version = "([^"]+)"/\1/p')
 
   echo "$(
     cat ${PKG_TEMPLATE_DIR}/${TEMPLATE} \
     | sed -r "s/{{NAME_DASHED}}/${NAME_DASHED}/g" \
     | sed -r "s/{{NAME_UNDERSCORED}}/${NAME_UNDERSCORED}/g" \
-    | sed -r "s/{{VERSION}}/${VERSION}/g"
-  )" > ${PACKAGE_DIR}/${TEMPLATE}
+    | sed -r "s/{{PKG_NAME}}/${PKG_NAME}/g" \
+    | sed -r "s/{{PKG_VERSION}}/${PKG_VERSION}/g"
+  )" > ${PKG_DIR}/${TEMPLATE}
 
 }
 
@@ -30,14 +32,14 @@ build_wasm_npm_pkg_for ()
   NAME_DASHED=$1
   NAME_UNDERSCORED=$(echo "${NAME_DASHED}" | sed -r 's/-/_/g')
 
-  PACKAGE_DIR=${NPM_DIR}/${NAME_DASHED}
+  PKG_DIR=${NPM_DIR}/${NAME_DASHED}
 
-  rm -rf ${PACKAGE_DIR}/{src,dist}
+  rm -rf ${PKG_DIR}/{src,dist}
 
   cd ${ROOT_DIR}
   cargo rustc -p ${NAME_DASHED} --target wasm32-unknown-unknown --features typescript --crate-type=cdylib --release
-  wasm-bindgen --target web ./target/wasm32-unknown-unknown/release/${NAME_UNDERSCORED}.wasm --out-dir ${PACKAGE_DIR}/src
-  wasm-opt ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}_bg.wasm -o ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}_bg.wasm -Oz
+  wasm-bindgen --target web ./target/wasm32-unknown-unknown/release/${NAME_UNDERSCORED}.wasm --out-dir ${PKG_DIR}/src
+  wasm-opt ${PKG_DIR}/src/${NAME_UNDERSCORED}_bg.wasm -o ${PKG_DIR}/src/${NAME_UNDERSCORED}_bg.wasm -Oz
   cd ~-
 
   write_template ${NAME_DASHED} ${NAME_UNDERSCORED} README.md
@@ -47,13 +49,13 @@ build_wasm_npm_pkg_for ()
   write_template ${NAME_DASHED} ${NAME_UNDERSCORED} src/index.js
 
   # commenting out all `new URL()` and `fetch()` calls for great compatibility with JS bundlers
-  sed -i.bkp -r 's;(input = new URL.+);//\1;g' ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}.js
-  sed -i.bkp -r 's;(input = fetch.+);//\1;g' ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}.js
-  rm ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}.js.bkp
+  sed -i.bkp -r 's;(input = new URL.+);//\1;g' ${PKG_DIR}/src/${NAME_UNDERSCORED}.js
+  sed -i.bkp -r 's;(input = fetch.+);//\1;g' ${PKG_DIR}/src/${NAME_UNDERSCORED}.js
+  rm ${PKG_DIR}/src/${NAME_UNDERSCORED}.js.bkp
 
-  pnpm -C ${PACKAGE_DIR} install
-  pnpm -C ${PACKAGE_DIR} build
-  pnpm -C ${PACKAGE_DIR} test
+  pnpm -C ${PKG_DIR} install
+  pnpm -C ${PKG_DIR} build
+  pnpm -C ${PKG_DIR} test
 }
 
 

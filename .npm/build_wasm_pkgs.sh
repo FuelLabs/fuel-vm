bc03989bf8e126e61e9edf276aeedcd0bf72d567
anderson <anderson@arboleya.me>
2023-07-19 07:07:15 -0300

    Renaming and moving script inside `.npm` dir (decluttering root dir)

--- .npm/build_wasm_pkgs.sh
+++ .npm/build_wasm_pkgs.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+NPM_DIR=${ROOT_DIR}/.npm
+
+
 write_template ()
 {
   NAME_DASHED=$1
   NAME_UNDERSCORED=$2
   TEMPLATE=$3
 
-  PKG_TEMPLATE_DIR=.npm/.pkg-template
-  PACKAGE_DIR=.npm/${NAME_DASHED}
+  PKG_TEMPLATE_DIR=${NPM_DIR}/.pkg-template
+  PACKAGE_DIR=${NPM_DIR}/${NAME_DASHED}
 
-  VERSION=$(cat ./Cargo.toml | sed -nr 's/^version = "([^"]+)"/\1/p')
+  VERSION=$(cat ${ROOT_DIR}/Cargo.toml | sed -nr 's/^version = "([^"]+)"/\1/p')
 
   echo "$(
     cat ${PKG_TEMPLATE_DIR}/${TEMPLATE} \
@@ -26,13 +30,15 @@ build_wasm_npm_pkg_for ()
   NAME_DASHED=$1
   NAME_UNDERSCORED=$(echo "${NAME_DASHED}" | sed -r 's/-/_/g')
 
-  PACKAGE_DIR=.npm/${NAME_DASHED}
+  PACKAGE_DIR=${NPM_DIR}/${NAME_DASHED}
 
   rm -rf ${PACKAGE_DIR}/{src,dist}
 
+  cd ${ROOT_DIR}
   cargo rustc -p ${NAME_DASHED} --target wasm32-unknown-unknown --features typescript --crate-type=cdylib --release
   wasm-bindgen --target web ./target/wasm32-unknown-unknown/release/${NAME_UNDERSCORED}.wasm --out-dir ${PACKAGE_DIR}/src
   wasm-opt ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}_bg.wasm -o ${PACKAGE_DIR}/src/${NAME_UNDERSCORED}_bg.wasm -Oz
+  cd -
 
   write_template ${NAME_DASHED} ${NAME_UNDERSCORED} README.md
   write_template ${NAME_DASHED} ${NAME_UNDERSCORED} package.json

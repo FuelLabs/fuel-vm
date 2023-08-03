93e1b63bcd5468959a09686c76070d8fcca2bd7f
green <xgreenx9999@gmail.com>
2023-08-03 23:48:47 +0100

    Fixed artifacts of rebasing

--- fuel-tx/src/transaction/consensus_parameters/gas.rs
+++ fuel-tx/src/transaction/consensus_parameters/gas.rs
@@ -1,10 +1,7 @@
 //! Tools for gas instrumentalization
 
-use std::{
-    ops::Deref,
-    sync::Arc,
-};
-
+use alloc::sync::Arc;
+use core::ops::Deref;
 use fuel_types::Word;
 
 #[allow(dead_code)]
@@ -145,11 +142,32 @@ impl GasUnit {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 /// Gas costings for every op.
 /// The inner values are wrapped in an [`Arc`]
 /// so this is cheap to clone.
 pub struct GasCosts(Arc<GasCostsValues>);
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for GasCosts {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        Ok(GasCosts(Arc::new(serde::Deserialize::deserialize(
+            deserializer,
+        )?)))
+    }
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for GasCosts {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serde::Serialize::serialize(self.0.as_ref(), serializer)
+    }
+}
 
 impl GasCosts {
     /// Create new cost values wrapped in an [`Arc`].

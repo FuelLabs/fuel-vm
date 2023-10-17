31483ed4c0ed4af252f724cd54dbf078350f24f8
Hannes Karppila <hannes.karppila@gmail.com>
2023-10-17 13:49:11 +0300

    fmt

--- fuel-vm/examples/external.rs
+++ fuel-vm/examples/external.rs
@@ -1,14 +1,44 @@
 //! This example shows how you can provide a custom ECAL instruction to the VM.
 //! Here we use it to provide a way to read from arbitrary files on the host machine.
 
-use std::{path::PathBuf, fs::{File, self}, io::{Seek, SeekFrom, Read}};
+use std::{
+    fs::{
+        self,
+        File,
+    },
+    io::{
+        Read,
+        Seek,
+        SeekFrom,
+    },
+    path::PathBuf,
+};
 
-use fuel_asm::{PanicReason, RegId, op, GTFArgs};
-use fuel_tx::{Script, TransactionBuilder, Finalizable, ConsensusParameters, Receipt};
-use fuel_vm::{prelude::{Interpreter, MemoryRange, MemoryClient, IntoChecked}, storage::MemoryStorage};
+use fuel_asm::{
+    op,
+    GTFArgs,
+    PanicReason,
+    RegId,
+};
+use fuel_tx::{
+    ConsensusParameters,
+    Finalizable,
+    Receipt,
+    Script,
+    TransactionBuilder,
+};
+use fuel_vm::{
+    prelude::{
+        Interpreter,
+        IntoChecked,
+        MemoryClient,
+        MemoryRange,
+    },
+    storage::MemoryStorage,
+};
 
 fn main() {
-    let mut vm: Interpreter<MemoryStorage, Script>  = Interpreter::with_memory_storage();
+    let mut vm: Interpreter<MemoryStorage, Script> = Interpreter::with_memory_storage();
     vm.set_ecal(|vm, a, b, c, d| {
         let a = vm.registers()[a]; // Seek offset
         let b = vm.registers()[b]; // Read length
@@ -24,28 +54,35 @@ fn main() {
 
         // Seek file to correct position
         let mut file = File::open(path).map_err(|_| PanicReason::EcalError)?;
-        let _ = file.seek(SeekFrom::Start(a)).map_err(|_| PanicReason::EcalError)?;
+        let _ = file
+            .seek(SeekFrom::Start(a))
+            .map_err(|_| PanicReason::EcalError)?;
 
-        // Allocate the requested buffer in vm memory and read directly from the file into it
+        // Allocate the requested buffer in vm memory and read directly from the file into
+        // it
         vm.allocate(b)?;
         let r = MemoryRange::new(vm.registers()[RegId::HP], b)?;
-        file.read(&mut vm.memory_mut()[r.usizes()]).map_err(|_| PanicReason::EcalError)?;
-        
+        file.read(&mut vm.memory_mut()[r.usizes()])
+            .map_err(|_| PanicReason::EcalError)?;
+
         Ok(())
     });
 
-
     let script_data: Vec<u8> = file!().bytes().collect();
-    let script = vec![
-        op::movi(0x20, 4), // Seek 4 bytes
-        op::movi(0x21, 8), // Read next 8 bytes
-        op::gtf_args(0x22, 0x00, GTFArgs::ScriptData), // File path is script_data
-        op::movi(0x23, script_data.len().try_into().unwrap()), // File path length
-        op::ecal(0x20, 0x21, 0x22, 0x23), // Read from file
-        op::lw(0x20, RegId::HP, 0), // Read the 8 bytes from the file into a register
-        op::log(0x20, RegId::ZERO, RegId::ZERO, RegId::ZERO), // Log the result
-        op::ret(RegId::ONE),
-    ].into_iter().collect();
+    let script =
+        vec![
+            op::movi(0x20, 4), // Seek 4 bytes
+            op::movi(0x21, 8), // Read next 8 bytes
+            op::gtf_args(0x22, 0x00, GTFArgs::ScriptData), /* File path is
+                                * script_data */
+            op::movi(0x23, script_data.len().try_into().unwrap()), // File path length
+            op::ecal(0x20, 0x21, 0x22, 0x23),                      // Read from file
+            op::lw(0x20, RegId::HP, 0), // Read the 8 bytes from the file into a register
+            op::log(0x20, RegId::ZERO, RegId::ZERO, RegId::ZERO), // Log the result
+            op::ret(RegId::ONE),
+        ]
+        .into_iter()
+        .collect();
 
     let mut client = MemoryClient::from_txtor(vm.into());
     let consensus_params = ConsensusParameters::standard();
@@ -60,7 +97,7 @@ fn main() {
     client.transact(tx);
     let receipts = client.receipts().expect("Expected receipts");
 
-    let Receipt::Log {ra, ..} = receipts.first().unwrap() else {
+    let Receipt::Log { ra, .. } = receipts.first().unwrap() else {
         panic!("Expected a log receipt");
     };
 

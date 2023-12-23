317f342bb30c657fac3f21ebf6c09b805f1a8b3b
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-12-23 18:08:58 -0500

    Clean up

--- fuel-merkle/src/sparse/merkle_tree.rs
+++ fuel-merkle/src/sparse/merkle_tree.rs
@@ -19,8 +19,6 @@ use crate::{
     },
 };
 
-use proptest_derive::Arbitrary;
-
 use crate::sparse::{
     branch::{
         merge_branches,
@@ -70,8 +68,7 @@ impl<StorageError> From<StorageError> for MerkleTreeError<StorageError> {
 /// The safe Merkle tree storage key prevents Merkle tree structure manipulations.
 /// The type contains only one constructor that hashes the storage key.
 #[derive(Clone, Copy, PartialEq, Eq)]
-// #[cfg(any(test, feature = "test-helpers"))]
-#[derive(Arbitrary)]
+#[cfg_attr(test, derive(proptest_derive::Arbitrary))]
 pub struct MerkleTreeKey(Bytes32);
 
 impl MerkleTreeKey {

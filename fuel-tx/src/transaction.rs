5bbddeee1ccd2013966f7a3703ca8c780e358de5
Turner <james.mitchell.turner@gmail.com>
2024-02-15 11:39:05 -0800

    Add other place

--- fuel-tx/src/transaction.rs
+++ fuel-tx/src/transaction.rs
@@ -921,6 +921,7 @@ pub mod typescript {
             output_contract: crate::output::contract::Contract,
             mint_amount: Word,
             mint_asset_id: AssetId,
+            gas_price: Word,
         ) -> Mint {
             Mint {
                 tx_pointer,
@@ -928,6 +929,7 @@ pub mod typescript {
                 output_contract,
                 mint_amount,
                 mint_asset_id,
+                gas_price,
                 metadata: None,
             }
         }

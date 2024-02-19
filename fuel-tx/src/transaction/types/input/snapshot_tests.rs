32e187e9417d14cb090a08f0e77a5f7ef7ffa79f
Turner <james.mitchell.turner@gmail.com>
2024-02-19 11:30:00 -0800

    Replace some tip stuff for serialization and snapshots

--- fuel-tx/src/transaction/types/input/snapshot_tests.rs
+++ fuel-tx/src/transaction/types/input/snapshot_tests.rs
@@ -19,6 +19,7 @@ fn tx_with_signed_coin_snapshot() {
             predicate: Empty::new(),
             predicate_data: Empty::new(),
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .witness_limit(1000)
@@ -44,6 +45,7 @@ fn tx_with_predicate_coin_snapshot() {
             predicate: vec![3u8; 10],
             predicate_data: vec![4u8; 12],
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .finalize_as_transaction();
@@ -63,6 +65,7 @@ fn tx_with_contract_snapshot() {
             tx_pointer: TxPointer::new(46.into(), 5),
             contract_id: [5u8; 32].into(),
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .finalize_as_transaction();
@@ -86,6 +89,7 @@ fn tx_with_signed_message_coin() {
             predicate: Empty::new(),
             predicate_data: Empty::new(),
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .witness_limit(1000)
@@ -110,6 +114,7 @@ fn tx_with_predicate_message_coin() {
             predicate: vec![7u8; 11],
             predicate_data: vec![8u8; 12],
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .finalize_as_transaction();
@@ -133,6 +138,7 @@ fn tx_with_signed_message_data() {
             predicate: Empty::new(),
             predicate_data: Empty::new(),
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .witness_limit(1000)
@@ -157,6 +163,7 @@ fn tx_with_predicate_message_data() {
             predicate: vec![7u8; 11],
             predicate_data: vec![8u8; 12],
         }))
+        .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
         .finalize_as_transaction();

53ef05bec9930af7cbfebede4252aebe56db54c6
Hannes Karppila <hannes.karppila@gmail.com>
2024-03-13 17:36:11 +0100

    Add test cases for tx validation

---
+++ fuel-vm/src/tests/limits.rs
@@ -0,0 +1,83 @@
+use core::str::FromStr;
+use fuel_asm::{
+    op,
+    RegId,
+};
+use fuel_tx::{
+    ConsensusParameters,
+    TransactionBuilder,
+};
+use fuel_types::BlockHeight;
+use fuel_vm::{
+    interpreter::InterpreterParams,
+    prelude::*,
+};
+use rand::{
+    rngs::StdRng,
+    Rng,
+    SeedableRng,
+};
+
+#[cfg(feature = "alloc")]
+use alloc::vec;
+
+#[test]
+fn cannot_exceed_max_inputs() {
+    let rng = &mut StdRng::seed_from_u64(1234);
+    let params = ConsensusParameters::default();
+
+    let mut script = TransactionBuilder::script(
+        vec![op::ret(RegId::ONE)].into_iter().collect(),
+        vec![],
+    );
+    for _ in 0..=params.tx_params.max_inputs {
+        script.add_input(Input::coin_signed(
+            rng.gen(),
+            rng.gen(),
+            0,
+            rng.gen(),
+            rng.gen(),
+            rng.gen(),
+        ));
+    }
+    let tx = script
+        .finalize()
+        .into_checked(0u32.into(), &params)
+        .expect_err("Tx is invalid and shouldn't validate");
+}
+
+#[test]
+fn cannot_exceed_max_outputs() {
+    let rng = &mut StdRng::seed_from_u64(1234);
+    let params = ConsensusParameters::default();
+
+    let mut script = TransactionBuilder::script(
+        vec![op::ret(RegId::ONE)].into_iter().collect(),
+        vec![],
+    );
+    for _ in 0..=params.tx_params.max_outputs {
+        script.add_output(Output::variable(rng.gen(), rng.gen(), rng.gen()));
+    }
+    let tx = script
+        .finalize()
+        .into_checked(0u32.into(), &params)
+        .expect_err("Tx is invalid and shouldn't validate");
+}
+
+#[test]
+fn cannot_exceed_max_witnesses() {
+    let rng = &mut StdRng::seed_from_u64(1234);
+    let params = ConsensusParameters::default();
+
+    let mut script = TransactionBuilder::script(
+        vec![op::ret(RegId::ONE)].into_iter().collect(),
+        vec![],
+    );
+    for _ in 0..=params.tx_params.max_witnesses {
+        script.add_witness(Witness::from(vec![rng.gen::<u8>(); 1]));
+    }
+    let tx = script
+        .finalize()
+        .into_checked(0u32.into(), &params)
+        .expect_err("Tx is invalid and shouldn't validate");
+}

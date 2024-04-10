e6aeef85bc1bbf7949f180867bcd3f69a8a06296
Green Baneling <XgreenX9999@gmail.com>
2024-04-10 22:25:38 +0200

    Update fuel-asm/src/panic_reason.rs
    Co-authored-by: Mitchell Turner <james.mitchell.turner@gmail.com>

--- fuel-asm/src/panic_reason.rs
+++ fuel-asm/src/panic_reason.rs
@@ -137,7 +137,7 @@ enum_from! {
         /// Attempting to read or write uninitialized memory.
         /// Also occurs when boundary crosses from stack to heap.
         UninitalizedMemoryAccess = 0x30,
-        /// It is not allowed to override the consensus parameters.
+        /// Overriding consensus parameters is not allowed.
         OverridingConsensusParameters = 0x31,
         /// The storage doesn't know about the hash of the state transition bytecode.
         UnknownStateTransactionBytecodeHash = 0x32,

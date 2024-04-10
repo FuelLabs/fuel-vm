ac6f16b90ddd8e805dc412c710bf1fd2524e932e
Max Ammann <max@maxammann.org>
2024-04-10 13:45:46 +0100

    Add initial fuzz files

---
+++ fuel-vm/fuzz/src/seed.rs
@@ -0,0 +1,42 @@
+use fuel_vm::consts::WORD_SIZE;
+use fuel_vm::fuel_asm::{Instruction, RawInstruction};
+use fuel_vm::fuel_crypto::rand::Rng;
+use fuel_vm::fuel_crypto::rand::SeedableRng;
+use fuel_vm::fuel_types::Word;
+use fuel_vm::prelude::SecretKey;
+use fuel_vm_fuzz::FuzzData;
+use fuel_vm_fuzz::{decode, decode_instructions, encode};
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() {
+    let input = std::env::args().nth(1).expect("no input path given");
+    let output = std::env::args().nth(2).expect("no output path given");
+    let paths = fs::read_dir(input).unwrap();
+
+    for path in paths {
+        let entry = path.unwrap();
+        let program = std::fs::read(entry.path()).unwrap();
+
+        println!("{:?}", entry.file_name().to_str().unwrap());
+
+        let data = FuzzData {
+            program,
+            sub_program: vec![],
+            script_data: vec![],
+        };
+
+        let encoded = encode(&data);
+        let decoded = decode(&encoded).unwrap();
+
+        if decoded != data {
+            println!("{:?}", data);
+            println!("{:?}", decoded);
+            panic!("mismatch")
+        }
+
+        fs::write(PathBuf::from(&output).join(entry.file_name()), &encoded).unwrap();
+    }
+}

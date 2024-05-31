efca5d3b41af78061673d54c0b7230dae8d85c62
Brandon Vrooman <brandon.vrooman@gmail.com>
2024-05-31 19:15:32 -0400

    Default exports

--- fuel-merkle/src/sparse.rs
+++ fuel-merkle/src/sparse.rs
@@ -4,16 +4,18 @@ mod primitive;
 
 pub(crate) use hash::zero_sum;
 
-pub use merkle_tree::{
-    MerkleTree,
-    MerkleTreeError,
-    MerkleTreeKey,
-};
-pub use primitive::Primitive;
 pub mod in_memory;
 pub mod proof;
 
 use crate::common::Bytes32;
+
+// Define default Merkle Tree structures as concrete implementations of generic
+// types, using 32 byte key sizes
+pub type MerkleTree<TableType, StorageType> =
+    merkle_tree::MerkleTree<32, TableType, StorageType>;
+pub type MerkleTreeError<StorageError> = merkle_tree::MerkleTreeError<32, StorageError>;
+pub type MerkleTreeKey = merkle_tree::MerkleTreeKey<32>;
+pub type Primitive = primitive::Primitive<32>;
 
 pub fn empty_sum() -> &'static Bytes32 {
     zero_sum()

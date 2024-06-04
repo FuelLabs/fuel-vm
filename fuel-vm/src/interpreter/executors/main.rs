0e7647d73ac18f27a3033fad7ed02375aabcb094
Green Baneling <XgreenX9999@gmail.com>
2024-06-04 23:56:08 +0200

    Make `VmMemoryPool::get_new` async (#748)
    * Make `VmMemoryPool::get_new` async
    
    * Updated CHANGELOG.md

--- fuel-vm/src/interpreter/executors/main.rs
+++ fuel-vm/src/interpreter/executors/main.rs
@@ -251,7 +251,7 @@ where
             {
                 let tx = kind.tx().clone();
                 let my_params = params.clone();
-                let mut memory = pool.get_new();
+                let mut memory = pool.get_new().await;
 
                 let verify_task = E::create_task(move || {
                     Interpreter::check_predicate(

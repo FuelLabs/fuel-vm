5d9087db8bc891d115a2f1f3d561940a50c3ce51
Hannes Karppila <hannes.karppila@gmail.com>
2024-09-04 05:18:19 +0300

    Also make compression context async

--- fuel-compression/src/traits.rs
+++ fuel-compression/src/traits.rs
@@ -25,7 +25,7 @@ where
     /// context. The context is mutable to allow for stateful compression.
     /// For instance, it can be used to extract original data when replacing it with
     /// references.
-    fn compress(&mut self, value: &Type) -> Result<Type::Compressed, Self::Error>;
+    async fn compress(&mut self, value: &Type) -> Result<Type::Compressed, Self::Error>;
 }
 
 /// A context that can be used to decompress a type.
@@ -39,7 +39,7 @@ where
 
     /// Perform decompression, returning the original data.
     /// The context can be used to resolve references.
-    fn decompress(&self, value: &Type::Compressed) -> Result<Type, Self::Error>;
+    async fn decompress(&self, value: &Type::Compressed) -> Result<Type, Self::Error>;
 }
 
 /// Error type for context errors.

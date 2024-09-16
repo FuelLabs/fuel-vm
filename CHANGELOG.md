ab468bf8b219afd43823bac460fd88694bb22b6a
Green Baneling <XgreenX9999@gmail.com>
2024-09-16 17:49:17 +0200

    Returned the old behaviour of the json serialization for policies (#823)
    * Returned the old behaviour of the json serialization for policies
    
    * Updated CHANGELOG.md
    
    * Update WASM tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 - [#825](https://github.com/FuelLabs/fuel-vm/pull/733): Avoid leaking partially allocated memory when array deserialization fails
 
 ### Changed
+- [#823](https://github.com/FuelLabs/fuel-vm/pull/823): Returned the old behaviour of the json serialization for policies.
 
 #### Breaking
 - [#826](https://github.com/FuelLabs/fuel-vm/pull/826): Skip the panic reason from canonical serialization of the panic receipt.
--- fuel-tx/src/transaction/types/input/snapshot_tests.rs
+++ fuel-tx/src/transaction/types/input/snapshot_tests.rs
@@ -1,12 +1,14 @@
 //! snapshot tests to ensure the serialized format of inputs doesn't change
 
 use super::*;
-use crate::TransactionBuilder;
+use crate::{
+    Transaction,
+    TransactionBuilder,
+};
 use fuel_types::canonical::Serialize;
 
-#[test]
-fn tx_with_signed_coin_snapshot() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_signed_coin_snapshot() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::CoinSigned(CoinSigned {
             utxo_id: UtxoId::new([1u8; 32].into(), 2),
             owner: [2u8; 32].into(),
@@ -22,7 +24,12 @@ fn tx_with_signed_coin_snapshot() {
         .maturity(123.into())
         .max_fee_limit(1000000)
         .witness_limit(1000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_signed_coin_snapshot_canonical() {
+    let tx = tx_with_signed_coin_snapshot();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
@@ -30,8 +37,15 @@ fn tx_with_signed_coin_snapshot() {
 }
 
 #[test]
-fn tx_with_predicate_coin_snapshot() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_signed_coin_snapshot_json() {
+    let tx = tx_with_signed_coin_snapshot();
+
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
+}
+
+fn tx_with_predicate_coin_snapshot() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::CoinPredicate(CoinPredicate {
             utxo_id: UtxoId::new([1u8; 32].into(), 2),
             owner: [2u8; 32].into(),
@@ -46,7 +60,12 @@ fn tx_with_predicate_coin_snapshot() {
         .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_predicate_coin_snapshot_canonical() {
+    let tx = tx_with_predicate_coin_snapshot();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
@@ -54,8 +73,15 @@ fn tx_with_predicate_coin_snapshot() {
 }
 
 #[test]
-fn tx_with_contract_snapshot() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_predicate_coin_snapshot_json() {
+    let tx = tx_with_predicate_coin_snapshot();
+
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
+}
+
+fn tx_with_contract_snapshot() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::Contract(Contract {
             utxo_id: UtxoId::new([1u8; 32].into(), 2),
             balance_root: [2u8; 32].into(),
@@ -66,7 +92,12 @@ fn tx_with_contract_snapshot() {
         .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_contract_snapshot_canonical() {
+    let tx = tx_with_contract_snapshot();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
@@ -74,8 +105,15 @@ fn tx_with_contract_snapshot() {
 }
 
 #[test]
-fn tx_with_signed_message_coin() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_contract_snapshot_json() {
+    let tx = tx_with_contract_snapshot();
+
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
+}
+
+fn tx_with_signed_message_coin() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::MessageCoinSigned(MessageCoinSigned {
             sender: [2u8; 32].into(),
             recipient: [3u8; 32].into(),
@@ -91,7 +129,12 @@ fn tx_with_signed_message_coin() {
         .maturity(123.into())
         .max_fee_limit(1000000)
         .witness_limit(1000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_signed_message_coin_canonical() {
+    let tx = tx_with_signed_message_coin();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
@@ -99,8 +142,15 @@ fn tx_with_signed_message_coin() {
 }
 
 #[test]
-fn tx_with_predicate_message_coin() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_signed_message_coin_json() {
+    let tx = tx_with_signed_message_coin();
+
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
+}
+
+fn tx_with_predicate_message_coin() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::MessageCoinPredicate(MessageCoinPredicate {
             sender: [2u8; 32].into(),
             recipient: [3u8; 32].into(),
@@ -115,7 +165,12 @@ fn tx_with_predicate_message_coin() {
         .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_predicate_message_coin_canonical() {
+    let tx = tx_with_predicate_message_coin();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
@@ -123,8 +178,14 @@ fn tx_with_predicate_message_coin() {
 }
 
 #[test]
-fn tx_with_signed_message_data() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_predicate_message_coin_json() {
+    let tx = tx_with_predicate_message_coin();
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
+}
+
+fn tx_with_signed_message_data() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::MessageDataSigned(MessageDataSigned {
             sender: [2u8; 32].into(),
             recipient: [3u8; 32].into(),
@@ -140,7 +201,12 @@ fn tx_with_signed_message_data() {
         .maturity(123.into())
         .max_fee_limit(1000000)
         .witness_limit(1000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_signed_message_data_canonical() {
+    let tx = tx_with_signed_message_data();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
@@ -148,8 +214,15 @@ fn tx_with_signed_message_data() {
 }
 
 #[test]
-fn tx_with_predicate_message_data() {
-    let tx = TransactionBuilder::script(vec![], vec![])
+fn tx_with_signed_message_data_json() {
+    let tx = tx_with_signed_message_data();
+
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
+}
+
+fn tx_with_predicate_message_data() -> Transaction {
+    TransactionBuilder::script(vec![], vec![])
         .add_input(Input::MessageDataPredicate(MessageDataPredicate {
             sender: [2u8; 32].into(),
             recipient: [3u8; 32].into(),
@@ -164,9 +237,23 @@ fn tx_with_predicate_message_data() {
         .tip(1)
         .maturity(123.into())
         .max_fee_limit(1000000)
-        .finalize_as_transaction();
+        .finalize_as_transaction()
+}
+
+#[test]
+fn tx_with_predicate_message_data_canonical() {
+    let tx = tx_with_predicate_message_data();
 
     let bytes = tx.to_bytes();
     let hex = hex::encode(bytes);
     insta::assert_snapshot!(hex);
+}
+
+#[test]
+fn tx_with_predicate_message_data_json() {
+    let tx = tx_with_predicate_message_data();
+
+    // Json
+    let json = serde_json::to_string_pretty(&tx).unwrap();
+    insta::assert_snapshot!(json);
 }

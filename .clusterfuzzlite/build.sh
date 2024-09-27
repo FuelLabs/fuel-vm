456fb4487ea49a467659f114b7e182c68da2837b
Mårten Blankfors <marten@blankfors.se>
2024-09-27 09:37:05 +0200

    chore: Remove flag needed for older compiler

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,7 +4,6 @@ cd $SRC/fuel-vm
 
 cd fuel-vm
 
-export CARGO_CFG_CURVE25519_DALEK_BACKEND=serial # This fixes building on nightly-2023-12-28-x86_64-unknown-linux-gnu, which is no longer compatible with the SIMD feature of curve25519; building on stable does not work because ASan is a dependency of coverage
 cargo fuzz build -O --sanitizer none
 
 cp fuzz/target/x86_64-unknown-linux-gnu/release/grammar_aware_advanced $OUT/

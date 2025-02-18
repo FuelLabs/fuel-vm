c33a925e164c79f51141ca0886aa96d5fc165129
Hannes Karppila <hannes.karppila@gmail.com>
2025-02-18 18:50:18 +0200

    Fix function name

--- fuel-vm/src/interpreter/gas/tests.rs
+++ fuel-vm/src/interpreter/gas/tests.rs
@@ -27,7 +27,7 @@ fn test_gas_charge(input: GasChargeInput) -> SimpleResult<GasChargeOutput> {
     } = input;
     let mut cgas = RegMut::new(&mut cgas);
     let mut ggas = RegMut::new(&mut ggas);
-    gas_charge_inner(cgas.as_mut(), ggas.as_mut(), dependent_factor).map(|_| {
+    gas_charge(cgas.as_mut(), ggas.as_mut(), dependent_factor).map(|_| {
         GasChargeOutput {
             cgas: *cgas,
             ggas: *ggas,
@@ -42,7 +42,7 @@ fn test_gas_charges_ggas_on_out_of_gas() {
     let gas = 20;
     let mut cgas = RegMut::new(&mut cgas);
     let mut ggas = RegMut::new(&mut ggas);
-    let _ = gas_charge_inner(cgas.as_mut(), ggas.as_mut(), gas)
+    let _ = gas_charge(cgas.as_mut(), ggas.as_mut(), gas)
         .expect_err("Gas charge should fail");
     assert_eq!(*ggas, 5);
     assert_eq!(*cgas, 0);

25ebd4232b7a3d332af00d95f15dd57f937b8bf5
Mitch Turner <james.mitchell.turner@gmail.com>
2025-04-18 14:20:14 -0600

    Add new GM arg with tests

--- fuel-vm/src/interpreter/metadata/tests.rs
+++ fuel-vm/src/interpreter/metadata/tests.rs
@@ -28,6 +28,7 @@ fn test_metadata() {
         imm,
         ChainId::default(),
         TxParameters::default().tx_offset() as Word,
+        0,
     )
     .unwrap();
     assert_eq!(pc, 8);
@@ -76,8 +77,48 @@ fn get_chain_id(context: Context, chain_id: u64) {
         imm,
         chain_id.into(),
         TxParameters::default().tx_offset() as Word,
+        0,
     )
     .unwrap();
 
     assert_eq!(result, chain_id);
+}
+
+// should fail in predicate estimation and predicate verification, but pass in other
+// contexts
+#[test_case(Context::PredicateEstimation { program: RuntimePredicate::empty() }, 2, Err(PanicReason::CanNotGetGasPriceInPredicate.into()); "can not fetch inside predicate estimation")]
+#[test_case(Context::PredicateVerification { program: RuntimePredicate::empty() }, 2, Err(PanicReason::CanNotGetGasPriceInPredicate.into()); "can not fetch inside predicate verification")]
+#[test_case(Context::Script { block_height: BlockHeight::default() }, 2, Ok(()); "can fetch inside script")]
+#[test_case(Context::Call { block_height: BlockHeight::default() }, 2, Ok(()); "can fetch inside call")]
+fn get_gas_price(context: Context, gas_price: u64, expected_res: SimpleResult<()>) {
+    let mut frames = vec![];
+    let mut pc = 4;
+    let mut result = 0;
+    let imm = GMArgs::GetGasPrice as Immediate18;
+
+    if context.is_internal() {
+        frames.push(CallFrame::default());
+    }
+
+    let actual_res = metadata(
+        &context,
+        &frames,
+        RegMut::new(&mut pc),
+        &mut result,
+        imm,
+        ChainId::default(),
+        TxParameters::default().tx_offset() as Word,
+        gas_price,
+    );
+
+    match expected_res {
+        Ok(_) => {
+            assert_eq!(actual_res, Ok(()));
+            assert_eq!(result, gas_price);
+        }
+        Err(e) => {
+            assert_eq!(actual_res, Err(e));
+            assert_eq!(result, 0);
+        }
+    }
 }

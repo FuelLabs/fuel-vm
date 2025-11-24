279fdf3cc7a811a2c66410221b4adf4ba0d82518
Saeed Dadkhah <sa.dadkhah@gmail.com>
2025-11-24 15:05:02 +0330

    remove uselss changes.

--- fuel-types/src/lib.rs
+++ fuel-types/src/lib.rs
@@ -21,12 +21,6 @@ extern crate alloc;
 extern crate core;
 
 pub mod canonical;
-pub use canonical::{
-    Deserialize,
-    DeserializeForwardCompatible,
-    Serialize,
-    SerializeForwardCompatible,
-};
 
 mod array_types;
 #[cfg(feature = "alloc")]

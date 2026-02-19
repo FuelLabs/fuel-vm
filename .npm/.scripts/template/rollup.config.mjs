db005021328e813679b4bfd85811769a549b1269
Mike Christopher <mchristopher@users.noreply.github.com>
2026-02-18 19:21:17 -0800

    fix: publish wasm package binary to npm

--- .npm/.scripts/template/rollup.config.mjs
+++ .npm/.scripts/template/rollup.config.mjs
@@ -1,27 +1,52 @@
 import { wasm } from '@rollup/plugin-wasm'
 import dts from 'rollup-plugin-dts'
+import { copyFileSync } from 'node:fs'
 
 const sync = ['src/{{NAME_UNDERSCORED}}_bg.wasm'];
 
+const copyWasm = {
+  name: 'copy-wasm',
+  writeBundle() {
+    copyFileSync('src/{{NAME_UNDERSCORED}}_bg.wasm', 'dist/{{NAME_UNDERSCORED}}_bg.wasm');
+  }
+};
+
 export default [
   {
-    plugins: [wasm({ sync, targetEnv: 'auto-inline' })],
+    plugins: [wasm({ sync, targetEnv: 'auto' })],
     input: 'src/index.js',
     output: [
       { file: 'dist/node/index.cjs', format: 'cjs' },
     ]
   }, {
-    plugins: [wasm({ sync, targetEnv: 'auto-inline' })],
+    plugins: [wasm({ sync, targetEnv: 'auto' })],
     input: 'src/index.js',
     output: [
       { file: 'dist/web/index.mjs', format: 'es' },
     ]
   }, {
+    input: 'src/index_slim.js',
+    output: [
+      { file: 'dist/slim/index.cjs', format: 'cjs' },
+    ]
+  }, {
+    plugins: [copyWasm],
+    input: 'src/index_slim.js',
+    output: [
+      { file: 'dist/slim/index.mjs', format: 'es' },
+    ]
+  }, {
     plugins: [dts()],
-    input: 'src/{{NAME_UNDERSCORED}}.d.ts',
+    input: 'src/index.d.ts',
     output: [
       { file: 'dist/web/index.d.ts', format: 'es' },
-      { file: 'dist/node/index.d.ts', format: 'es' }
+      { file: 'dist/node/index.d.ts', format: 'es' },
+    ],
+  }, {
+    plugins: [dts()],
+    input: 'src/index_slim.d.ts',
+    output: [
+      { file: 'dist/slim/index.d.ts', format: 'es' },
     ],
   }
 ]
